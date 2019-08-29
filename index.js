661c935b08cc54db1dc2d7e028c7f8fb63fb3fa8
sg0891039 <jim.resnowski@sabre.com>
2019-08-29 16:13:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ app.post('/', upload.single('thumb'), async (req, res, next) => {
   const isAudio = (payload.Metadata.librarySectionType === 'artist');
   const key = sha1(payload.Server.uuid + payload.Metadata.ratingKey);
 
+  console.log('[What is user]', `it is  ${payload.user}`);
   // missing required properties
   if (!payload.user || !payload.Metadata || !(isAudio || isVideo)) {
     return res.sendStatus(400);
