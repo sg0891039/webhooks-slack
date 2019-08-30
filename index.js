df8695842fba227ee652d387fabc867c84c2d17c
sg0891039 <jim.resnowski@sabre.com>
2019-08-30 15:22:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -86,10 +86,10 @@ app.post('/', upload.single('thumb'), async (req, res, next) => {
 
   // post to slack
   if ((payload.event === 'media.scrobble' && isVideo) || payload.event === 'media.rate' || payload.event === 'library.new') {
-    if (payload.event != 'library.new') {
-      const location = await getLocation(payload.Player.publicAddress);
-    } else {
+    if (payload.event === 'library.new') {
       const location = null;
+    } else {
+      const location = await getLocation(payload.Player.publicAddress);
     }
     console.log('[SLACK]', `the event is ${payload.event}`);
     let action;
